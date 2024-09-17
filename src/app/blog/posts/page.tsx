14e5a12ce90d440728ae95942d4cbf0c994238a7
Roka <kazakov302215@gmail.com>
2024-09-16 20:40:29 -0600

    Connected to db

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,10 +1,17 @@
-import { posts } from '@/app/lib/placeholder-data';
-import Post from '@/app/ui/components/posts/Post';
+import { posts } from '@/app/lib/placeholder-data'
+import Post from '@/app/ui/components/posts/Post'
+import { connectToDB } from '@/app/lib/data'
 
-export default function Page() {
+export default async function Page() {
+  const client = await connectToDB()
+
   return (
     <>
+      {client && <p className='text-sky-500'>Connected to database</p>}
       <h1>Posts</h1>
-      {posts.map((post) => <Post key={post.id} {...post} />)}
-    </>)
+      {posts.map(post => (
+        <Post key={post.id} {...post} />
+      ))}
+    </>
+  )
 }
